a83a6fd4473227c0d24cba48864f08fae0fd4de6
Anish Ojha <anishojha82@gmail.com>
2020-05-16 16:19:04 +0530

    added map functionality

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,95 +4,50 @@ import cx from "classnames";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 
-const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
-  if (!confirmed) {
+// const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+const Cards = ({ data }) => {
+  let keys;
+  if (!Object.keys(data).length) {
     return "Loading...";
+  } else {
+    keys = Object.keys(data).filter((item) => item !== "lastUpdate");
   }
 
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid
-          item
-          component={Card}
-          xs={12}
-          md={3}
-          className={cx(styles.card, styles.infected)}
-        >
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Infected
-            </Typography>
-            <Typography variant="h5">
-              <Countup
-                start={0}
-                end={confirmed.value}
-                duration={2.5}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography>
-            <Typography variant="body2">
-              Number of active cases for COVID-19
-            </Typography>
-          </CardContent>
-        </Grid>
-        <Grid
-          item
-          component={Card}
-          xs={12}
-          md={3}
-          className={cx(styles.card, styles.recovered)}
-        >
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Recovered
-            </Typography>
-            <Typography variant="h5">
-              <Countup
-                start={0}
-                end={recovered.value}
-                duration={2.5}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography>
-            <Typography variant="body2">
-              Number of recovered cases from COVID-19
-            </Typography>
-          </CardContent>
-        </Grid>
-        <Grid
-          item
-          component={Card}
-          xs={12}
-          md={3}
-          className={cx(styles.card, styles.deaths)}
-        >
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Deaths
-            </Typography>
-            <Typography variant="h5">
-              <Countup
-                start={0}
-                end={deaths.value}
-                duration={2.5}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography>
-            <Typography variant="body2">
-              Number of deaths due to COVID-19
-            </Typography>
-          </CardContent>
-        </Grid>
+        {keys.map((item, i) => {
+          return (
+            <Grid
+              item
+              component={Card}
+              xs={12}
+              md={3}
+              className={cx(styles.card, styles[item])}
+              key={i}
+            >
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  {item.toUpperCase()}
+                </Typography>
+                <Typography variant="h5">
+                  <Countup
+                    start={0}
+                    end={data[item].value}
+                    duration={2.5}
+                    separator=","
+                  />
+                </Typography>
+                <Typography color="textSecondary">
+                  {new Date(data.lastUpdate).toDateString()}
+                </Typography>
+                <Typography variant="body2">
+                  Number of {item} cases for COVID-19
+                </Typography>
+              </CardContent>
+            </Grid>
+          );
+        })}
       </Grid>
     </div>
   );
